b2663b59bd87c0d86d8f3cbfcb279f64bb74b5ed
harfe <9767572+harfe@users.noreply.github.com>
2018-05-14 11:30:56 +0200

    added drive_with_turn and stop() methods

--- ev3_commands.py
+++ ev3_commands.py
@@ -9,20 +9,81 @@ class EV3Command(ev3.EV3):
     EV3Command implements some commands of the EV3
     """
 
-    def beep(self):
+    def beep(self) -> None:
         """Produce a beep with 440 hz at volume 20 for 1 second."""
         self.play_tone(440,1000,20)
-        return
 
-    def play_tone(self,frequency,duration,vol=50):
+    def play_tone(self,frequency:int ,duration: float,vol=50) -> None:
         """Play a tone with the specified frequency, duration in seconds,
         and volume.
         """
         milli_seconds = int(duration*1000)
         # TODO: check possible frequency range
-        # TODO: check input for ranges
+        # TODO: check if input is valid, convert to int if necessary
         ops=b''.join([ev3.opSound,ev3.TONE,ev3.LCX(vol),ev3.LCX(frequency),
                       ev3.LCX(milli_seconds),])
         self.send_direct_cmd(ops)
+
+    def drive_with_turn(self,speed: int, turn: int):
+        """
+        Start unlimited movement of the vehicle
+
+        Arguments:
+        speed: speed in percent [-100 - 100]
+          > 0: forward
+          < 0: backward
+        turn: type of turn [-200 - 200]
+          -200: circle right on place
+          -100: turn right with unmoved right wheel
+           0  : straight
+           100: turn left with unmoved left wheel
+           200: circle left on place
+        """
+        assert self._sync_mode != ev3.SYNC, \
+                'no unlimited operations allowed in sync_mode SYNC'
+        assert isinstance(speed, int), \
+                'speed needs to be an integer value'
+        assert -100 <= speed and speed <= 100, \
+                "speed needs to be in range [-100 - 100]"
+        assert isinstance(turn, int), \
+                'turn needs to be an integer value'
+        assert -200 <= turn and turn <= 200, \
+                'turn needs to be in range [-200 - 200]'
+        ports = ev3.PORT_A+ev3.PORT_D
+        ops = b''.join([
+            ev3.opOutput_Step_Sync,
+            ev3.LCX(0),              # LAYER
+            ev3.LCX(ports),          # NOS
+            ev3.LCX(speed),
+            ev3.LCX(turn),
+            ev3.LCX(0),              # STEPS
+            ev3.LCX(0),              # BRAKE
+            ev3.opOutput_Start,
+            ev3.LCX(0),              # LAYER
+            ev3.LCX(ports)           # NOS
+        ])
+        self.send_direct_cmd(ops)
         return
 
+
+    def stop(self, brake: bool=False) -> None:
+        """
+        Stop movement of the vehicle
+
+        Arguments:
+        brake: flag if activating brake
+        """
+        assert isinstance(brake, bool), "brake needs to be a boolean value"
+        if brake:
+            brake_int = 1
+        else:
+            brake_int = 0
+        ports = ev3.PORT_A+ev3.PORT_D
+        ops = b''.join([
+            ev3.opOutput_Stop,
+            ev3.LCX(0),                                  # LAYER
+            ev3.LCX(ports), # NOS
+            ev3.LCX(brake_int)                           # BRAKE
+        ])
+        self.send_direct_cmd(ops)
+        return
