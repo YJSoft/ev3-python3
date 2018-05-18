64d0cb481745f3d3af76c1065595dcb16b12c14c
saxophon <39300292+saxophon@users.noreply.github.com>
2018-05-18 16:30:58 +0200

    Feedbacksteuerung

--- feedback_control.py
+++ feedback_control.py
@@ -28,17 +28,35 @@ tolerance = 7
 tolerance2 = 12
 anzahlgrenze = 30
 
+k=-0.7
 
 # pos=e.get_wheel_position()
 # print("wheel position = "+str(pos))
 anzahl=0
 anzahl2=0
 
+e.drive_with_turn(100,200)
+time.sleep(0.6)
+e.drive_with_turn(-100,200)
+time.sleep(0.6)
+e.drive_with_turn(10,200)
+e.play_tone(512,0.1,1)
+time.sleep(0.5)
+
+
 while True:
     j.update()
     v = j.get_axis_position(0, 0)
-    v = int(v)
+    #v = int(v)
     rate=e.get_gyro_rate()
+    v += k*rate
+    v=int(v)
+    if v>100:
+        v=100
+    if v<-100:
+        v=-100
+    if -5 <= v and v <= 5:
+        v=0
     milli_seconds=int(time.time()*1000)%(60*1000)
     print('v=%4d, rate=%4d ms=%5d' % (v,rate,milli_seconds) )
     if v==0:
