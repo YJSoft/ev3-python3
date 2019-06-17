ae02c8b7a20d546ca07e6ee33c4774a5eeacbefd
YJSoft <YJSoft@users.noreply.github.com>
2019-06-17 15:29:18 +0900

    Update www.py

--- www.py
+++ www.py
@@ -4,7 +4,7 @@ wwwapp = Flask(__name__)
 
 my_ev3 = ev3.EV3(protocol=ev3.BLUETOOTH, host='00:16:53:4f:28:a6')
 my_music = ev3_sound.Jukebox(ev3_obj=my_ev3)
-my_vehicle = ev3_vehicle.TwoWheelVehicle(ev3_obj=my_ev3)
+my_vehicle = ev3_vehicle.TwoWheelVehicle(radius_wheel=0.056,tread=0.028,ev3_obj=my_ev3)
 
 @wwwapp.route("/")
 def rootpage():
