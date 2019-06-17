0f7b9bf8ac183e065095a3da3dba90992e20ab0c
YJSoft <YJSoft@users.noreply.github.com>
2019-06-17 15:19:29 +0900

    Update www.py

--- www.py
+++ www.py
@@ -1,17 +1,34 @@
-from ev3 import EV3
+import ev3, ev3_sound, ev3_vehicle
 from flask import Flask
 wwwapp = Flask(__name__)
 
-ev3 = EV3()
+my_ev3 = ev3.EV3(protocol=ev3.BLUETOOTH, host='00:16:53:4f:28:a6')
+my_music = ev3_sound.Jukebox(ev3_obj=my_ev3)
+my_vehicle = ev3_vehicle.TwoWheelVehicle(ev3_obj=my_ev3)
 
 @wwwapp.route("/")
 def rootpage():
-    return "VoiceModule v0.1 by Robogram!"
+    return "EV3 WWW Gateway v0.1 by Robogram!"
 
 @wwwapp.route("/moveSync/<type>/<port>/<speed>")
 def cmd_move(type, port, speed):
     try:
-        pass
+        my_vehicle.port_left = ev3.PORT_A
+        my_vehicle.port_right = ev3.PORT_D
+        if port == "port_bc"
+            my_vehicle.port_left = ev3.PORT_B
+            my_vehicle.port_right = ev3.PORT_C
+        
+        if type == "forward":
+            my_vehicle.move(speed, 0)
+        elif type == "backward":
+            my_vehicle.move(speed * -1, 0)
+        elif type == "left":
+            my_vehicle.move(speed, 200)
+        elif type == "right":
+            my_vehicle.move(speed, -200)
+        else:
+            pass
     except Exception:
         return "Failure"
     return "Success"
